f3058ae0474b935fd9ed9e22b9e3b9f4ad74420e
Alvagit25 <alvarodesegovia@gmail.com>
2022-02-07 12:24:28 +0100

    primer commit

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  ParseIntPipe,
+  Get,
+  Param,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { Player } from './player.entity';
 import { PlayerService } from './player.service';
 
 @Controller('player')
@@ -9,5 +17,17 @@ export class PlayerController {
   getAll() {
     //return 'Get all players';
     return this.playerService.getPlayers();
+  }
+
+  @Get(':id')
+  getPlayer(@Param('id', ParseIntPipe) id: Player) {
+    return this.playerService.getPlayer(id);
+  }
+
+  @Post()
+  createPlayer(@Body() player) {
+    //return player;
+    //return `Crear un jugador: ${player.name} Juega de ${player.position}`;
+    return this.playerService.createPlayer(player);
   }
 }
--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -21,6 +21,17 @@ export class PlayerService {
   ];
 
   getPlayers(): Player[] {
+    //return 'Estos son los jugadores';
     return this.players;
+  }
+  getPlayer(id): Player {
+    return this.players.find((player) => player.id == id);
+  }
+  createPlayer(player): Player {
+    this.players.push({
+      id: this.players.length + 1,
+      ...player,
+    });
+    return this.players[this.players.length - 1];
   }
 }
